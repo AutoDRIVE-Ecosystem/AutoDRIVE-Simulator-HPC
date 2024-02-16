5ad63d8d32c9b622f91ee91119b00930de55c272
rein5 <martin.173@outlook.com>
2024-02-16 13:16:16 -0500

    minor fix

--- record_simulation.sh
+++ record_simulation.sh
@@ -7,13 +7,15 @@ if [ "$#" -ne 2 ]; then
 fi
 output_filename=$1
 simulation_duration=$2
-output_directory="/home/AutoDRIVE_Simulator/output"
+# Get the directory where the script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+output_directory="$script_dir/output"
 
 # Create virtual display (:2) with screen resolution (1920x1080x24)
-Xvfb :2 -screen 0 1920x1080x24 &
+Xvfb :97 -screen 0 1920x1080x24 &
 
 # Export the display environment variable to point to the virtual display
-export DISPLAY=:2
+export DISPLAY=:97
 
 # Launch the AutoDRIVE Simulator application with output directed to the virtual display
 ./AutoDRIVE\ Simulator.x86_64 >/dev/null 2>&1 &
@@ -22,7 +24,7 @@ export DISPLAY=:2
 sleep 5
 
 # Record video from the virtual display using ffmpeg and save it to a video file
-ffmpeg -f x11grab -video_size 1920x1080 -i :2 -t $simulation_duration -c:v libx264 -preset ultrafast -qp 0 "$output_directory/$output_filename"
+ffmpeg -f x11grab -video_size 1920x1080 -i :97 -t $simulation_duration -c:v libx264 -preset ultrafast -qp 0 "$output_directory/$output_filename"
 
 # Inform user about the completion of the recording
 echo "Video recording completed and saved to $output_directory/$output_filename"
