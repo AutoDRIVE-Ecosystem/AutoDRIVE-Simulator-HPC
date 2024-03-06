c142583e7aa0aead72f7177eb757c466bf99b09d
rein5 <martin.173@outlook.com>
2024-03-06 04:26:40 -0500

    add weather and time control to webviewer palmetto test

--- palmetto/autodrive_test_webviewer/run_test_instance.sh
+++ palmetto/autodrive_test_webviewer/run_test_instance.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 echo "Starting AutoDRIVE Simulator container sandbox..."
-singularity instance start --nv -B $HOME,$TMPDIR /home/giovanm/autodrive_simulator_autoconnect_xvfb/ inst1
+singularity instance start --nv -B $HOME,$TMPDIR $SIMULATOR_DIR inst1
 
-echo "Launching AutoDRIVE Simulator and recording video to file..."
-cd /home/giovanm/autodrive_simulator_autoconnect_xvfb/home/AutoDRIVE_Simulator/
-singularity exec -B $HOME,$TMPDIR,/home/giovanm/autodrive_simulator_autoconnect_xvfb/home/AutoDRIVE_Simulator/output instance://inst1 bash ./simulation_stream_recorder.sh stream output_$PBS_ARRAY_INDEX >/home/giovanm/autodrive_test_webviewer/sim_out_$PBS_ARRAY_INDEX.log 2>&1 &
+echo "Launching AutoDRIVE Simulator and streaming its video output..."
+cd $SIMULATOR_DIR/home/AutoDRIVE_Simulator/
+singularity exec -B $HOME,$TMPDIR,$SIMULATOR_DIR/home/AutoDRIVE_Simulator/output \
+instance://inst1 bash ./simulation_stream_recorder.sh stream output_$PBS_ARRAY_INDEX \
+> $TEST_DIR/sim_out_$PBS_ARRAY_INDEX.log 2>&1 &
 
 echo "Waiting for 20 seconds before running the python server..."
 sleep 20
 
 echo "Running the python server..."
-python -u /home/giovanm/autodrive_opencav/AutoDRIVE-AVLDC/aeb_py/opencav_aeb.py
+python -u $TEST_DIR/opencav_aeb.py $PBS_ARRAY_INDEX
 
 echo "Done"
