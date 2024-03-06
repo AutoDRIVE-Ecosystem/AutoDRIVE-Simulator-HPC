fe3c89faacc15d70c30fb846c14360ef1a7f2ca6
rein5 <martin.173@outlook.com>
2024-03-06 00:26:00 -0500

    minor cleanup

--- deploy_webviewer.sh
+++ deploy_webviewer.sh
@@ -7,7 +7,7 @@ module add anaconda3/2022.05-gcc/9.5.0
 TARGET_DIR="/home/giovanm/autodrive_simulator_autoconnect_xvfb/home/AutoDRIVE_Simulator/output"
 
 # Copy index.html to the target directory, replacing it if it exists
-cp -f -r index.html loadStreams.js style.css images "$TARGET_DIR"
+cp -f -r index.html loadStreams.js config.json style.css images "$TARGET_DIR"
 
 # Start an HTTP server on port 8000 in the background
 cd "$TARGET_DIR"
