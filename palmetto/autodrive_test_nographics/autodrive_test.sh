3d83db6e6f1f5b25d806120f8ba11a168ed88af1
rein5 <martin.173@outlook.com>
2024-03-07 02:34:33 -0500

    clean up nographics palmetto test

--- palmetto/autodrive_test_nographics/autodrive_test.sh
+++ palmetto/autodrive_test_nographics/autodrive_test.sh
@@ -2,16 +2,18 @@
 
 #PBS -N autodrive_test
 #PBS -l select=1:ngpus=2:ncpus=16:mpiprocs=16:mem=120gb:interconnect=hdr,walltime=0:02:00
-#PBS -J 1-10
+#PBS -J 1-16
 #PBS -m abe
 #PBS -M giovanm@clemson.edu
 #PBS -j oe
 
-cd /home/giovanm/autodrive_test_nographics
+export TEST_DIR=/home/giovanm/autodrive_test_nographics/
+export SIMULATOR_DIR=/home/giovanm/autodrive_simulator_autoconnect_weather_time/
+export XDG_RUNTIME_DIR=/tmp/runtime-dir
+
+# Activate the autodrive conda environment
 module add anaconda3/2022.05-gcc/9.5.0
 source activate autodrive
 
-export XDG_RUNTIME_DIR=/tmp/runtime-dir
-
-./run_test_instance.sh
-
+# Run test instance
+$TEST_DIR/run_test_instance.sh
--- palmetto/autodrive_test_nographics/run_test_instance.sh
+++ palmetto/autodrive_test_nographics/run_test_instance.sh
@@ -2,11 +2,12 @@
 
 echo "Launching AutoDRIVE Simulator..."
 # Run simulator in the background 
-/home/giovanm/autodrive_autoconnect/AutoDRIVE\ Simulator.x86_64 -batchmode -nographics >/dev/null 2>&1 &
+$SIMULATOR_DIR/home/AutoDRIVE_Simulator/AutoDRIVE\ Simulator.x86_64 -batchmode -nographics >/dev/null 2>&1 &
+
 echo "Waiting for 20 seconds before running the python server..."
 sleep 20
 
 echo "Running the python server..."
-python -u /home/giovanm/autodrive_opencav/AutoDRIVE-AVLDC/aeb_py/opencav_aeb.py
+python -u $TEST_DIR/opencav_aeb.py
 
 echo "Done"
